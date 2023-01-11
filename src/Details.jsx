fe0d075ad66ab5b7a72c4ae0647008f5cff5e6cc
Muhammad Hanzla <mhanzla1408@gmail.com>
2023-01-11 10:37:31 +0500

    first commit

---
+++ src/Details.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+import { useParams } from "react-router-dom"
+
+const Details = () => {
+  const { id } = useParams();
+  return (
+    <h2>{id}</h2>
+  )
+}
+
+export default Details
---
+++ src/useBreedList.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react"
+
+const localCache = {
+    // "bird": ["dsdfa", "asdsad", ],
+    // "cat": ["adfa", "adsf"],
+}
+
+const useBreedList = (animal) => {
+    const [breedList, setBreedList] = useState([]);
+
+    useEffect(() => {
+        if (!animal) {
+            setBreedList([]);
+        } else if (localCache[animal]) {
+            setBreedList(localCache[animal])
+        } else {
+            getBreedList();
+        }
+
+        async function getBreedList() {
+            setBreedList([]);
+
+            const res = await fetch(
+                `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+            );
+            const json = await res.json();
+            localCache[animal] = json.breeds || [];
+            setBreedList(localCache[animal]);
+            console.log(breedList)
+            console.log(localCache)
+        }
+    }, [animal])
+    return [breedList]
+}
+
+export default useBreedList
